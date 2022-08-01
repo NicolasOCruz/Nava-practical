2abac157f2460b1946d6f793b138acdd0162dd2f
NicolasOCruz <nicolaspereira244@gmail.com>
2022-08-01 00:23:23 -0300

    Refactoring service and controller

--- src/main/java/com/example/api/service/CustomerService.java
+++ src/main/java/com/example/api/service/CustomerService.java
@@ -1,30 +1,41 @@
 package com.example.api.service;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.example.api.domain.Customer;
+import com.example.api.dto.CustomerDTO;
+import com.example.api.repository.CustomerRepository;
+import com.example.api.service.exception.ObjectNotFoundException;
+import com.example.api.util.FunctionsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.api.domain.Customer;
-import com.example.api.repository.CustomerRepository;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
 
-	private CustomerRepository repository;
+    private CustomerRepository repository;
 
-	@Autowired
-	public CustomerService(CustomerRepository repository) {
-		this.repository = repository;
-	}
+    @Autowired
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
-	public List<Customer> findAll() {
-		return repository.findAllByOrderByNameAsc();
-	}
+    public List<CustomerDTO> findAll() {
+        List<Customer> customers = repository.findAllByOrderByNameAsc();
+        if (FunctionsUtil.isEmpty(customers))
+            return new ArrayList<>();
+        return customers
+                .stream()
+                .map(customer -> new CustomerDTO(customer.getName(), customer.getEmail()))
+                .collect(Collectors.toList());
+    }
 
-	public Optional<Customer> findById(Long id) {
-		return repository.findById(id);
-	}
+    public CustomerDTO findById(Long id) {
+        Customer customer = repository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Customer from id " + id + " not found."));
+        return new CustomerDTO(customer.getName(), customer.getEmail());
+    }
 
 }
--- src/main/java/com/example/api/web/rest/CustomerController.java
+++ src/main/java/com/example/api/web/rest/CustomerController.java
@@ -1,38 +1,35 @@
 package com.example.api.web.rest;
 
-import java.util.List;
-
+import com.example.api.dto.CustomerDTO;
+import com.example.api.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-import com.example.api.domain.Customer;
-import com.example.api.service.CustomerService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
-	private CustomerService service;
+    private CustomerService service;
 
-	@Autowired
-	public CustomerController(CustomerService service) {
-		this.service = service;
-	}
+    @Autowired
+    public CustomerController(CustomerService service) {
+        this.service = service;
+    }
 
-	@GetMapping
-	public List<Customer> findAll() {
-		return service.findAll();
-	}
+    @GetMapping
+    public ResponseEntity<List<CustomerDTO>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
+    }
 
-	@GetMapping("/{id}")
-	public Customer findById(@PathVariable Long id) {
-		return service.findById(id)
-				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Customer not found"));
-	}
-
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.findById(id));
+    }
 }
